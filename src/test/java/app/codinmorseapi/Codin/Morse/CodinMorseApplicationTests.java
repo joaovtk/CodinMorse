d763bbb9ceaca47732060b5282b1cf7312ff2abc
claudio silva <zrafaelsilva8@gmail.com>
2025-04-23 16:07:06 -0300

    Initial Commit

---
+++ src/test/java/app/codinmorseapi/Codin/Morse/CodinMorseApplicationTests.java
@@ -0,0 +1,13 @@
+package app.codinmorseapi.Codin.Morse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinMorseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
